9d3fe76399916c7890ec5cf91e04bb64fbd502a2
Hariharan <harimaniam@gmail.com>
2023-02-26 17:58:12 +0000

    HH01: Updated solution after white board analysis.

--- src/main/java/com/jlr/assignment/RomanNumberTranslator.java
+++ src/main/java/com/jlr/assignment/RomanNumberTranslator.java
@@ -8,17 +8,22 @@ import java.util.Map;
 public class RomanNumberTranslator {
 
     /**
+     * Catalog to initialize mapping values of Base10 to Roman numbers
+     */
+    private static final Map<Integer, Map<Integer, String>> catalog = Map.of(
+            0, Map.of( 0, "", 1, "I", 2, "II", 3, "III", 4, "IV", 5, "V", 6, "VI", 7, "VII", 8, "VIII", 9, "IX"),
+            1, Map.of( 0, "", 1, "X", 2, "XX", 3, "XXX", 4, "XL", 5, "L", 6, "LX", 7, "LXX", 8, "LXXX", 9, "XC"),
+            2, Map.of( 0, "", 1, "C", 2, "CC", 3, "CCC", 4, "CD", 5, "D", 6, "DC", 7, "DCC", 8, "DCCC", 9, "CM"),
+            3, Map.of( 0, "", 1, "M", 2, "MM", 3, "MMM"));;
+
+    /**
      * Main method to translate Base10 to Roman number
      */
-    private Map<Integer, Map<Integer, String>> catalog;
-
-
     public String translate(final int baseTenNumber) throws IllegalArgumentException {
 
         if ( baseTenNumber < 1 || baseTenNumber > 3999) {
             throw new IllegalArgumentException("Number should be in between 0 and 4000");
         } else {
-            this.catalog = InitCatalog();
             return translateBase10(0, baseTenNumber);
         }
     }
@@ -36,21 +41,6 @@ public class RomanNumberTranslator {
             sb.append(catalog.get(base).get(number % 10));
         }
         return sb.toString();
-    }
-
-
-    /**
-     * to initialize the values for catolog
-     * In future we could retrieve these values from DB
-     * @return
-     */
-    protected Map InitCatalog(){
-        return Map.of(
-                0, Map.of( 0, "", 1, "I", 2, "II", 3, "III", 4, "IV", 5, "V", 6, "VI", 7, "VII", 8, "VIII", 9, "IX"),
-                1, Map.of( 0, "", 1, "X", 2, "XX", 3, "XXX", 4, "XL", 5, "L", 6, "LX", 7, "LXX", 8, "LXXX", 9, "XC"),
-                2, Map.of( 0, "", 1, "C", 2, "CC", 3, "CCC", 4, "CD", 5, "D", 6, "DC", 7, "DCC", 8, "DCCC", 9, "CM"),
-                3, Map.of( 0, "", 1, "M", 2, "MM", 3, "MMM"));
-
     }
 
 }
